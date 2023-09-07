6b1ac7230a3a56ce56784a9563c7dc53489fe594
unknown <brunnosantana98@gmail.com>
2023-09-07 12:53:19 -0300

    initial commit

---
+++ src/main/java/br/com/brunno/offermanager/OfferManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunno.offermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunno/offermanager/OfferManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunno.offermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
